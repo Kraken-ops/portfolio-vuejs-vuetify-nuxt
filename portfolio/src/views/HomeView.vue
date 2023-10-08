47ff49a55cb173f2b174d1a1c807ffe0c49c8427
Kraken-ops <nildey21@gmail.com>
2023-10-09 02:05:43 +0530

    added files

---
+++ portfolio/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background}">
+    <navbar-component />
+  </v-app>
+  
+</template>
+
+<script>
+import NavbarComponent from '../components/NavbarComponent.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+        NavbarComponent,
+    },
+  }
+</script>
