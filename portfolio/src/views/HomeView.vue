770db600f1330c57baf958571181bd0983a981d1
Kraken-ops <nildey21@gmail.com>
2023-10-09 02:40:44 +0530

    changes in home component

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,6 +1,25 @@
 <template>
-  <v-app :style="{ background: $vuetify.theme.themes.dark.background}">
+  <v-app id="home" :style="{ background: $vuetify.theme.themes.dark.background}">
     <navbar-component />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-img src="1b.png" contain max-height="500"></v-img>
+        </v-col>
+        <v-col cols="6">
+          <h3 class="red--text text-darken-4 top">Hello I am Priyanshu</h3>
+          <h1 class="white--text">Front-end Developer</h1>
+          <p class="grey--text">
+            I am a computer science graduate working as a Front-end Developer.<br>
+            My passion for technology and eagerness to learn new things has driven me to stay upto date with the
+latest industry trends and tools. <br> I have experience working with popular web frameworks such as Vue, Nuxt, React
+Express, and Node.js, and I am always looking for opportunities to expand my skillset. <br>
+As a quick learner with a strong work ethic, I am confident that I can make a valuable contribution to your
+team. I am excited about the opportunity to apply my skills and knowledge to real world projects, and I am eager to take on new challenges as a software developer.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
   
 </template>
@@ -16,3 +35,8 @@ import NavbarComponent from '../components/NavbarComponent.vue'
     },
   }
 </script>
+<style scoped>
+.top{
+  margin-top: 180px;
+}
+</style>
